3b60ea5f427e6fd41d570c371253c2d436f47253
RamyaMahendran <ramyamahendran19@gmail.com>
2015-11-22 14:49:50 -0800

    a

---
+++ r/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace r
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.s = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.txtb1 = new System.Windows.Forms.TextBox();
+            this.txtb2 = new System.Windows.Forms.TextBox();
+            this.txtb3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 6000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(0, 0);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(75, 23);
+            this.b1.TabIndex = 0;
+            this.b1.Text = "b1";
+            this.b1.UseVisualStyleBackColor = true;
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(81, 12);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(75, 23);
+            this.b2.TabIndex = 1;
+            this.b2.Text = "b2";
+            this.b2.UseVisualStyleBackColor = true;
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(172, 30);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(75, 23);
+            this.b3.TabIndex = 2;
+            this.b3.Text = "b3";
+            this.b3.UseVisualStyleBackColor = true;
+            // 
+            // s
+            // 
+            this.s.Location = new System.Drawing.Point(92, 226);
+            this.s.Name = "s";
+            this.s.Size = new System.Drawing.Size(75, 23);
+            this.s.TabIndex = 3;
+            this.s.Text = "s";
+            this.s.UseVisualStyleBackColor = true;
+            this.s.TextChanged += new System.EventHandler(this.s_TextChanged_1);
+            this.s.Click += new System.EventHandler(this.s_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 3000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // txtb1
+            // 
+            this.txtb1.Location = new System.Drawing.Point(0, 33);
+            this.txtb1.Name = "txtb1";
+            this.txtb1.Size = new System.Drawing.Size(48, 20);
+            this.txtb1.TabIndex = 4;
+            // 
+            // txtb2
+            // 
+            this.txtb2.Location = new System.Drawing.Point(81, 57);
+            this.txtb2.Name = "txtb2";
+            this.txtb2.Size = new System.Drawing.Size(48, 20);
+            this.txtb2.TabIndex = 5;
+            // 
+            // txtb3
+            // 
+            this.txtb3.Location = new System.Drawing.Point(172, 85);
+            this.txtb3.Name = "txtb3";
+            this.txtb3.Size = new System.Drawing.Size(48, 20);
+            this.txtb3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtb3);
+            this.Controls.Add(this.txtb2);
+            this.Controls.Add(this.txtb1);
+            this.Controls.Add(this.s);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button s;
+        private System.Windows.Forms.TextBox txtb1;
+        public System.Windows.Forms.Timer timer1;
+        public System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox txtb2;
+        private System.Windows.Forms.TextBox txtb3;
+    }
+}
+
---
+++ r/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace r
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch stopwatch1 = new Stopwatch();
+        Stopwatch stopwatch2 = new Stopwatch();
+        Stopwatch stopwatch3 = new Stopwatch();
+        int turn = -1;
+        int p;
+        Button[] btns;
+        int[] Stage1;
+        int[] Stage2;
+        int[] Stage3;
+        int[] Stage4;
+        int[] Stage5;
+        int[] Stage6;
+        int[] queue;
+        int[] arrival;
+        Random rnd = new Random();
+  
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            btns = new Button[] { b1, b2, b3 };
+            Stage1 = new int[] { 0, 0, 1 };
+            Stage2 = new int[] { 0, 1, 1 };
+            Stage3 = new int[] { 1, 1, 1 };
+            Stage4 = new int[] { 0, 1, 0 };
+            Stage5 = new int[] { 1, 0, 1 };
+            Stage6 = new int[] { 1, 1, 0 };
+
+            arrival=new int []{5,5,5};
+
+            queue = new int[] { 0, 1, 2 };
+        }
+        void LightGlow(int[] arr)
+        {
+            queuemtd();
+            stopwatch1.Stop(); stopwatch2.Stop(); stopwatch3.Stop();
+           // MessageBox.Show(stopwatch3.Elapsed.Seconds.ToString());
+            txtb1.Text = stopwatch1.Elapsed.Seconds.ToString();
+            txtb2.Text = stopwatch2.Elapsed.Seconds.ToString();
+            txtb3.Text = stopwatch3.Elapsed.Seconds.ToString();
+            queue[0] = (queue[0] - (stopwatch1.Elapsed.Seconds/2));
+            queue[1] = (queue[1] - (stopwatch2.Elapsed.Seconds/2));
+            queue[2] = (queue[2] - (stopwatch3.Elapsed.Seconds/2));
+
+            stopwatch1.Reset();
+            stopwatch2.Reset();
+            stopwatch3.Reset();
+
+            for (int i = 0; i < 3; i++)
+            {
+                
+
+                if (arr[i] == 1)
+                {
+                   // stopwatch1.Reset();
+                   if (i == 0) { stopwatch1.Start();}
+                   else if (i == 1) { stopwatch2.Start(); }
+                   else { stopwatch3.Start(); }
+
+
+                    btns[i].BackColor = Color.Green;
+                  // queue[i] = queue[i] - 2;  
+                }   
+                else
+                {
+                    btns[i].BackColor = Color.Red;                
+                }
+               
+                btns[i].Text = queue[i].ToString();
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            turn = turn + 1;
+            p = (turn % 6) + 1;
+            s.Text = p.ToString();
+        }
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void s_TextChanged_1(object sender, EventArgs e)
+        {
+            switch (p)
+            {
+                case 1: LightGlow(Stage1); break;
+                case 2: LightGlow(Stage2); break;
+                case 3: LightGlow(Stage3); break;
+                case 4: LightGlow(Stage4); break;
+                case 5: LightGlow(Stage5); break;
+                case 6: LightGlow(Stage6); break;
+                default: break;
+            }
+        }
+        
+
+        public void timer2_Tick(object sender, EventArgs e)
+        {
+           
+        }
+        
+        public void queuemtd()
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+               // int n = rnd.Next(1, 3);
+                queue[i] = (queue[i] + 3);
+                // MessageBox.Show(queue[i].ToString() + "sd  " + i.ToString());
+                if (queue[i] > 10)
+                {
+                    queue[i] = 10;
+                }
+                btns[i].Text = queue[i].ToString();
+                //MessageBox.Show(queue[i].ToString() + "sd  " + i.ToString());
+                if (queue[i] == 0)
+                {
+                    arrival[i] = 0;
+                }
+                else
+                    if (btns[i].BackColor == Color.Green)
+                    {
+                        arrival[i] = 0;
+                    }
+                    else
+                    {
+                        arrival[i] = 1;
+                    }
+               // MessageBox.Show(queue[i].ToString() + arrival[i].ToString());
+            }
+        }
+
+        private void s_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
